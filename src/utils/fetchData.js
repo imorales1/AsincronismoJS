5eea224e6eccb0580f524a35d3046cec2ef1fa5f
imorales1 <alejan_dro-morales@hotmail.com>
2022-05-12 00:02:09 -0600

    Consumo de api con promesas

---
+++ src/utils/fetchData.js
@@ -0,0 +1,19 @@
+//Instanciamos nuestra libreria recien instalada
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+//Creamos una función que nos permitira traer la información desde nuestra API
+const fetchData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (() => {
+        if(xhttp.readyState === 4) {
+            (xhttp.status === 200) ? resolve(JSON.parse(xhttp.responseText)) : reject(new Error('Error', url_api))
+        }
+    });
+
+    xhttp.send();
+    });
+}
+
+module.exports = fetchData;
